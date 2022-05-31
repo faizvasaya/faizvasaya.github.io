e7f193bb28523044cfb71716b0804d038eddb8ae
Faizal Vasaya <faizalvasaya@Faizals-Macbook.local>
2022-05-31 17:35:24 +0530

    feat: Added sidebar and project setup

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,85 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    {/* Sidebar Logo */}
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    {/* Side bar middle menu items */}
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    {/* Side bar bottom menu items */}
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/faizalvasaya/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/FaizalVasaya"
+        >
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/faizvasaya/"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://medium.com/@faizvasya"
+        >
+          <FontAwesomeIcon icon={faMedium} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
