c3c19e1fdb785483742da09ded6c5a020034ea3d
Faizal Vasaya <faizalvasaya@Faizals-Macbook.local>
2022-06-11 12:59:43 +0530

    feat: Added about page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,129 @@
+import { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'contact_service',
+        'contact_form',
+        refForm.current,
+        '-_0yN3WBECfQB4nE5'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again!')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="user_name"
+                    placeholder="Name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="user_email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="tel"
+                    name="contact_number"
+                    placeholder="Contact number with country code"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required />
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Faizal Vasaya,
+          <br />
+          Neptune Society, Kalyani Nagar,
+          <br />
+          Pune - 411006
+          <br />
+          Maharastra, India
+          <br />
+          <br />
+          <a href="mailto:faizvasya@gmail.com">faizvasya@gmail.com</a>
+        </div>
+        <div className="map-wrap">
+          <MapContainer
+            center={[18.54985927339364, 73.90123225532982]}
+            zoom={13}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[18.54985927339364, 73.90123225532982]}>
+              <Popup>Faizal lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
